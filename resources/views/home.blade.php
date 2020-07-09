e36241ae6706af8ff91dbca3556f872675f603e9
Qasim <muhammadqasimaslam3@gmail.com>
2020-07-09 22:38:40 +0500

    HTML to Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.font')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    Some content
+@endsection
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('scripts')
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
